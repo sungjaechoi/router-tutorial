44c42fcd7cec8d0636d99da51123edb561b1aa97
sungjaechoi <sungjaechoi39@gmail.com>
2023-11-10 02:04:08 +0900

    useParams practice_setting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import About from "./pages/About";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile"
 
 function App() {
-  // console.log(<About/>)
-  // console.log(<Profile/>)
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile/>}/>
-    </Routes>
+    
   );
 }
 
