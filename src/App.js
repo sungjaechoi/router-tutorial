844505f41eb59260ff69ed87fc3a0be81775eb6d
sungjaechoi <sungjaechoi39@gmail.com>
2023-11-14 18:36:28 +0900

    useParams 연습2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
 
 function App() {
   return (
-    
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/categorys/:categoryID" element={<Category/>}/>
+    </Routes>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,24 @@
-
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return(
-    
+    <div>
+      <h1>"Welcome to the Home Page!"</h1>
+      <ul>
+        <li>
+          <Link to="/categorys/1">category1</Link>
+        </li>
+        <li>
+          <Link to="/categorys/2">category2</Link>
+        </li>
+        <li>
+          <Link to="/categorys/3">category3</Link>
+        </li>
+        <li>
+          <Link to="/categorys/4">category4</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
