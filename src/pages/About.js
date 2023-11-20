1d1cbe7e94677a3ef7ae75991db2ed82ba7b6df3
sungjaechoi <sungjaechoi39@gmail.com>
2023-11-21 07:41:57 +0900

    useLocation+useSearchParams
    useLocation과 useSearchParams 같이 확인해 보기

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,15 @@
+import { useLocation } from "react-router-dom";
 import {useSearchParams} from "react-router-dom";
 
 const About = () => {
+  const location = useLocation()
+  const {pathname, search, key} = location
+  
   const [searchParams, setSearchParams] = useSearchParams()
+  
 
   const detail = searchParams.get('detail')
   const mode = searchParams.get('mode')
-  console.log('detail', detail)
-  console.log('mode',mode)
 
   const onToggleDetail = () => {
     setSearchParams({mode, detail: detail === 'true' ? false : true})
@@ -20,8 +23,13 @@ const About = () => {
     <div>
       <h1>소개</h1>
       <p>리액트 라우터를 사용해 보는 프로젝트 입니다.</p>
+      <strong>useSearchParams</strong>
       <p>detail : {detail}</p>
       <p>mode : {mode}</p>
+      <strong>uselocation</strong>
+      <p>현재 URL 경로 : {pathname}</p>
+      <p>현재 URL의 쿼리스트링 : {search}</p>
+      <p>key : {key}</p>
       <button onClick={onToggleDetail} >Toggle detail</button>
       <button onClick={onIncreaseMode}>mode + 1</button>
     </div>
