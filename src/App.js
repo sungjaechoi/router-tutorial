089e345bb90be322e9cda1f83faa9d37128ae7e9
sungjaechoi <sungjaechoi39@gmail.com>
2023-11-25 02:49:02 +0900

    중첩 라우트 사용 전

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile"
+import Articles from "./pages/Articles";
+import Article from "./pages/Article";
 
 function App() {
   // console.log(<About/>)
@@ -11,6 +13,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/profiles/:username" element={<Profile/>}/>
+      <Route path="/articles" element={<Articles/>}/>
+      <Route path="/articles/:id" element={<Article/>}/>
     </Routes>
   );
 }
---
+++ src/pages/Articles.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+
+const Articles = () => {
+
+  return(
+    <ul>
+      <li>
+        <Link to="/articles/1">게시글 1</Link>
+      </li>
+      <li>
+        <Link to="/articles/2">게시글 2</Link>
+      </li>
+      <li>
+        <Link to="/articles/2">게시글 2</Link>
+      </li>
+    </ul>
+  )
+}
+
+export default Articles;
