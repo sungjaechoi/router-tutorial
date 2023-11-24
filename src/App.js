d6acbd62d46ff30c191db3b5f94439a661627aa1
sungjaechoi <sungjaechoi39@gmail.com>
2023-11-25 05:11:47 +0900

    index props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile"
@@ -10,11 +11,14 @@ function App() {
   // console.log(<Profile/>)
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile/>}/>
+      <Route element={<Layout/>}>
+        {/* index Props = path='/' 동일한 의미와 역활을 가짐 -> 좀더 명시적인 표현방법 */}
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile/>}/>
       <Route path="/articles" element={<Articles/>}>
         <Route path=":id" element={<Article/>}/>
+      </Route>
       </Route>
     </Routes>
   );
