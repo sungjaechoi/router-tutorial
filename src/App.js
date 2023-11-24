bb84001ea7b58a43a5dd2dd30250ca8b23f2023a
sungjaechoi <sungjaechoi39@gmail.com>
2023-11-25 03:47:33 +0900

    라우트 중첩과 Outlet 컴포넌트 적용

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/profiles/:username" element={<Profile/>}/>
-      <Route path="/articles" element={<Articles/>}/>
-      <Route path="/articles/:id" element={<Article/>}/>
+      <Route path="/articles" element={<Articles/>}>
+        <Route path=":id" element={<Article/>}/>
+      </Route>
     </Routes>
   );
 }
